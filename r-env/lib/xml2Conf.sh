2a18d756455271c0b6d3ae95b90d150c8ff9d6f0
AdrianForsythe <adrian.e.forsythe@gmail.com>
2021-11-29 10:42:43 +0100

    adding work from trial run, pre and post antibiotic bears

---
+++ r-env/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/crex/proj/sllstore2017021/nobackup/ADRIAN/metagenomics-power-analyses-tutorial/r-env/lib"
+XML2_LIBS="-lxml2 -L/crex/proj/sllstore2017021/nobackup/ADRIAN/metagenomics-power-analyses-tutorial/r-env/lib -lz -L/crex/proj/sllstore2017021/nobackup/ADRIAN/metagenomics-power-analyses-tutorial/r-env/lib -llzma -lpthread -L/crex/proj/sllstore2017021/nobackup/ADRIAN/metagenomics-power-analyses-tutorial/r-env/lib -liconv -L/crex/proj/sllstore2017021/nobackup/ADRIAN/metagenomics-power-analyses-tutorial/r-env/lib -licui18n -licuuc -licudata -lm "
+XML2_INCLUDEDIR="-I/crex/proj/sllstore2017021/nobackup/ADRIAN/metagenomics-power-analyses-tutorial/r-env/include/libxml2 -I/crex/proj/sllstore2017021/nobackup/ADRIAN/metagenomics-power-analyses-tutorial/r-env/include"
+MODULE_VERSION="xml2-2.9.12"
+
